222dd1456e910eb13e53b13a80e38cc41a0048a2
Jess <jnoriegaless@gmail.com>
2019-07-19 23:59:48 +0000

    Responsiveness. Some coding clean up. Some jQuery fixes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,22 +5,34 @@
  */
 
 //  iterate through tweets
-const renderTweets = function(tweets) {
+const renderTweets = function(tweets, effect) {
   for (let tweet of tweets) {
     let $newTweet = createTweetElement(tweet);
-    $(".tweet-container").append($newTweet);
+    if (effect) {
+      $newTweet.hide();
+    }
+    $(".tweet-container").prepend($newTweet);
   }
 }
 
 // 
 const createTweetElement = function(tweet) {
   // set tags
-  let $post_container = $('<article>');
+  let $post_container = $('<article class="tweet">');
   let $handle = $('<a>');
   let $avatar = `<img src="${tweet.user.avatars}" />`;
   let $name = $('<h1>');
   let $post = $('<p>');
   let $date = $('<h2>');
+  let $icons = `<img src="${'../images/icons.png'}" />`;
+
+  let $header = $('<header>');
+  let $body = $('<body>');
+  let $footer = $('<footer>');
+
+  $header.append($handle, $avatar, $name);
+  $body.append($post);
+  $footer.append($date, $icons);
 
   // calling value from tweets object
   $handle.text(tweet.user.handle);
@@ -30,11 +42,9 @@ const createTweetElement = function(tweet) {
 
   // append to post_container class
   $post_container
-    .append($handle)
-    .append($avatar)
-    .append($name)
-    .append($post)
-    .append($date)
+    .append($header)
+    .append($body)
+    .append($footer)
 
   return $post_container;
 }
@@ -42,14 +52,15 @@ const createTweetElement = function(tweet) {
 // existing tweets. tweets already submitted
 const loadTweets = function() {
   $.get("/tweets").then((result) => {
-    renderTweets(result);
+    renderTweets(result, false);
   });
 }
 
 // current tweets
 const loadCurrentTweet = function() {
   $.get("/tweets").then((result) => {
-    renderTweets([result[result.length - 1]]);
+    renderTweets([result[result.length - 1]], true);
+    $('article:first').slideDown('slow', () => {})
   });
 }
 
@@ -77,6 +88,8 @@ $(document).ready(function() {
       $.post( "/tweets", $(event.target).serialize()).then(() => {
         $("#error-message").slideUp().removeClass('show_error_msg');
         loadCurrentTweet();
+        $('.new-tweet form textarea').val('');
+        $('.counter').text(140);
       });
     }
 
--- public/scripts/scroll.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  
-  $('nav #right').click(function() {
-    $('html, body').animate({
-      scrollTop: $(".new-tweet").offset().top - 145
-    }, 900)
-  })
-  
-});
